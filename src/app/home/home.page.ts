a2e79a75a31e8abaf7acf1bd8974e0eacde781e1
wiwoduoc <am.veas@duocuc.cl>
2023-10-16 22:07:22 -0300

    Animacion

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { AnimationController, Animation } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,15 +8,36 @@ import { Router } from '@angular/router';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  constructor(private router: Router, private animationCtrl: AnimationController) { }
 
-  constructor(private router:Router) {}
+  ngAfterViewInit() {
+    this.playAnimation('.img'); // Animación para la imagen
+    this.playAnimation('.texto'); // Animación para el texto
+  }
 
   cargarLogin() {
-    this.router.navigateByUrl('/login')
+    this.router.navigateByUrl('/login');
+    this.playAnimation('.img');
+    this.playAnimation('.texto');
   }
 
   cargarRegistro() {
-    this.router.navigateByUrl('/register')
+    this.router.navigateByUrl('/register');
+    this.playAnimation('.img');
+    this.playAnimation('.texto');
   }
 
+  playAnimation(elementSelector: string) {
+    const element = document.querySelector(elementSelector);
+
+    if (element) {
+      const animation: Animation = this.animationCtrl.create()
+        .addElement(element)
+        .duration(1000)
+        .fromTo('opacity', 0, 1)
+        .fromTo('transform', 'translateX(-100%)', 'translateX(0)');
+
+      animation.play();
+    }
+  }
 }
