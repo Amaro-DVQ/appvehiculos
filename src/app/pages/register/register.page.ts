8e6c33f23f96fc7a487bbc0428f5441b73e81d84
Kensel Campusano <ken.campusano@duocuc.cl>
2023-10-18 15:56:38 -0300

    firebase conectada

--- src/app/pages/register/register.page.ts
+++ src/app/pages/register/register.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-register',
@@ -8,20 +9,44 @@ import { Router } from '@angular/router';
 })
 export class RegisterPage implements OnInit {
 
+  email:string = "";
   pass:string = "";
   confirmarPass:string = "";
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private userService:UserService) { }
 
   ngOnInit() {
   }
 
   register(){
-    if(this.pass == this.confirmarPass && this.pass != "" && this.confirmarPass != ""){
-    console.log("Usuario registrado")
-    }else{
+    if(this.pass != this.confirmarPass){
       console.log("Las contraseñas no coinciden")
+      return;
+    } else if(this.pass.length < 6){
+      console.log("La contraseña debe tener al menos 6 caracteres")
+      return;
+    } else if(this.email == ""){
+      console.log("El email no puede estar vacío")
+      return;
+    } else if(this.pass == ""){
+      console.log("La contraseña no puede estar vacía")
+      return;
+    } else if(this.confirmarPass == ""){
+      console.log("La confirmación de la contraseña no puede estar vacía")
+      return;
+    } else if(this.pass == this.confirmarPass){
+      console.log("Las contraseñas coinciden")
+    } else {
+      console.log("Error desconocido")
+      return;
     }
+
+    this.userService.register({email:this.email, password:this.pass}).then((res)=>{
+      console.log(res)
+      this.router.navigateByUrl('/home')
+    }).catch((err)=>{
+      console.log(err)
+    })
   }
 
   atrasInicio() {
--- src/app/services/helper.service.ts
+++ src/app/services/helper.service.ts
@@ -14,4 +14,5 @@ export class HelperService {
 
     await alert.present()
   }
+
 }
