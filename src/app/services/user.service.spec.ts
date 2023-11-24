57246ac6e9a6825c25a8ac055f6e3e12866a85e6
wiwoduoc <am.veas@duocuc.cl>
2023-11-24 04:37:44 -0300

    24-11

--- src/app/services/user.service.spec.ts
+++ src/app/services/user.service.spec.ts
@@ -1,12 +1,20 @@
 import { TestBed } from '@angular/core/testing';
-
 import { UserService } from './user.service';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { environment } from 'src/environments/environment';
 
 describe('UserService', () => {
   let service: UserService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        AngularFireModule.initializeApp(environment.firebaseConfig),
+        AngularFireAuthModule,
+      ],
+      providers: [UserService],
+    });
     service = TestBed.inject(UserService);
   });
 
